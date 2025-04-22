e22d38f630a844db392f9d31ee64b7292199063f
olivia-dp <olivia.osypovych@gmail.com>
2025-04-22 17:06:19 +0300

    fix styles

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,7 +2,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-// import { toast } from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 import { loginThunk } from "../../redux/auth/operations.js";
 import { loginSchema } from "../../schemas/schemas.js";
@@ -14,6 +14,7 @@ import { FaEnvelope, FaLock } from "react-icons/fa";
 const LoginForm = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+ 
 
   const handleSubmit = async (values, { setSubmitting, resetForm }) => {
     try {

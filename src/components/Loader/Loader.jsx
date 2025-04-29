fd3b5d524dc0a8e0627dc4eb05169acffbc4116c
Serhii Mordvaniuk <sm636332@gmail.com>
2025-04-29 15:33:55 +0200

    created loader and fix 404 page

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,12 @@
+import { ScaleLoader } from "react-spinners";
+import s from "./Loader.module.css";
+
 const Loader = () => {
-  return <div></div>;
+  return (
+    <div className={s.loader}>
+      <ScaleLoader color="#9e40ba" height={50} margin={4} />
+    </div>
+  );
 };
 
 export default Loader;

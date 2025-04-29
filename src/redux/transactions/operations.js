5959d633f3d043cda86915a5f6da2bdb8599749d
Serhii Mordvaniuk <sm636332@gmail.com>
2025-04-29 19:25:52 +0200

    fix delete funk redux

--- src/redux/transactions/operations.js
+++ src/redux/transactions/operations.js
@@ -31,7 +31,7 @@ export const editTransaction = createAsyncThunk(
   "transactions/editTransaction",
   async ({ id, date, type, category, comment, sum }, thunkAPI) => {
     try {
-      const { data } = await moneyGuardAPI.patch(/transactions/`${id}`, {
+      const { data } = await moneyGuardAPI.patch(`/transactions/${id}`, {
         date,
         type,
         category,
@@ -49,7 +49,7 @@ export const deleteTransaction = createAsyncThunk(
   "transactions/deleteTransaction",
   async (id, thunkAPI) => {
     try {
-      await moneyGuardAPI.delete(/transactions/`${id}`);
+      await moneyGuardAPI.delete(`/transactions/${id}`);
       toast.success("Transaction deleted successfully!");
       return id;
     } catch (error) {
